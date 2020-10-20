28a37637146471be46655d75c0886da9d3b629c7
pablorm296 <pablo.reyes.moctezuma@gmail.com>
2020-10-20 12:22:54 -0500

    Defined setup (setuptools)

---
+++ Src/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name = "SnapBot",
+    author = "Pablo Reyes Moctezuma",
+    author_email = "pablo.reyes.moctezuma@gmail.com",
+    package_dir = {
+        "SnapBot": "SnapBot",
+    }
+)
