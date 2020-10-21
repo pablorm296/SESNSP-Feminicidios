ed9f24367bddef3c686d0d09141c0a07fe8a84fb
pablorm296 <pablo.reyes.moctezuma@gmail.com>
2020-10-21 17:56:31 -0500

    Test version of main script

---
+++ Src/SnapBot/main.py
@@ -0,0 +1,5 @@
+from SnapBot.Bot import SnapBot
+
+myBot = SnapBot("https://www.gob.mx/sesnsp/articulos/informacion-sobre-violencia-contra-las-mujeres-incidencia-delictiva-y-llamadas-de-emergencia-9-1-1-febrero-2019", "/home/pakal/Documents/wgetTest/test2")
+
+myBot.getFullPage()
