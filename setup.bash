c0f9ceea0b9de5642d18e124bb066d71adc03d05
pablorm296 <pablo.reyes.moctezuma@gmail.com>
2020-10-21 18:36:10 -0500

    Added install script

---
+++ setup.bash
@@ -0,0 +1,14 @@
+
+# Make sure that we have the last version of the repo
+echo "Updating repo..."
+git pull
+
+# Create a virtualenv
+echo "Creating virtualenv..."
+virtualenv Env
+
+# Activate virtualenv
+source Env/bin/activate
+
+# Install requirements
+pip install -r requirements.txt
