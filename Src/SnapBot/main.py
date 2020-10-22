b61b38e347739c45b3bd061ba45310a78b4b1aba
pablorm296 <pablo.reyes.moctezuma@gmail.com>
2020-10-22 00:49:58 -0500

    Finished main routine

--- Src/SnapBot/main.py
+++ Src/SnapBot/main.py
@@ -1,5 +1,14 @@
 from SnapBot.Bot import SnapBot
 
+import logging
+
+logging.basicConfig(level = logging.INFO)
+
+# Define snapbot
 myBot = SnapBot("https://www.gob.mx/sesnsp/articulos/informacion-sobre-violencia-contra-las-mujeres-incidencia-delictiva-y-llamadas-de-emergencia-9-1-1-febrero-2019", "/srv/SESNSP/Data")
 
+# Get fullpage
+myBot.getFullPage()
+
+# Get document
 myBot.getDocument()
