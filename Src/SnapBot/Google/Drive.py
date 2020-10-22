f63dc5a7ead24a5ac7adbcebd2537b9ffa2889d7
pablorm296 <pablo.reyes.moctezuma@gmail.com>
2020-10-22 00:17:28 -0500

    Added methods to download drive document

--- Src/SnapBot/Google/Drive.py
+++ Src/SnapBot/Google/Drive.py
@@ -1,3 +1,4 @@
+import requests
 import re
 
 # Function that gets document id from url
@@ -15,3 +16,32 @@ def get_document_id(url:str):
         return match_results[0]
     else:
         return match_results
+
+def download_document(id, destination):
+    URL = "https://docs.google.com/uc?export=download"
+
+    session = requests.Session()
+
+    response = session.get(URL, params = { 'id' : id }, stream = True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = { 'id' : id, 'confirm' : token }
+        response = session.get(URL, params = params, stream = True)
+
+    save_response_content(response, destination)    
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+
+    return None
+
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
